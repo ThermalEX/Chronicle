cd372486258258e65f3cc766a798fc526ce0ee7c
ThermalEX <2210692765@qq.com>
2026-09-08 23:35:24 +0800

    feat: support portable repository storage

---
+++ apps/desktop/src-tauri/src/storage_root.rs
@@ -0,0 +1,13 @@
+use std::{io, path::{Path, PathBuf}};
+
+pub(crate) fn resolve_repository_root(executable: &Path, app_local_data: &Path) -> io::Result<PathBuf> {
+    let executable_directory = executable
+        .parent()
+        .ok_or_else(|| io::Error::other("executable has no parent directory"))?;
+
+    if executable_directory.join("portable.marker").is_file() {
+        Ok(executable_directory.join("Chronicle-data"))
+    } else {
+        Ok(app_local_data.join("Chronicle"))
+    }
+}
